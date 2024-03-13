c035d07820e2393c31f4a64cdd351c339a8377b8
dracomalfoydaw <djbudoykokey@gmail.com>
2024-03-13 08:33:37 +0800

    init commit

---
+++ res_cons.php
@@ -0,0 +1,13 @@
+<?php 
+define('CNF_APPNAME','Monitoring');
+define('CNF_APPNAME_CODE','monitoring');
+define('CNF_APPDESC','Monitoring');
+define('CNF_FOOTER','Monitoring');
+
+
+define('CNF_DBHOST','localhost');
+define('CNF_DBUSERNAME','root');
+define('CNF_DBPASS','1234');
+define('CNF_DBNAME','vle');
+define('CNF_DBDRIVER','mysqli');
+?>
