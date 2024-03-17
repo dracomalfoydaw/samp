155cc3a4361a9582253df7472e4c3728491d0ee9
dracomalfoydaw <djbudoykokey@gmail.com>
2024-03-18 06:45:05 +0800

    get user account

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -4,7 +4,7 @@ class User_model extends CI_Model {
 
     public function insertUser($username, $password, $email, $groupID, $profileID, $firstName, $lastName, $active, $entryBy) {
         $sql = "CALL InsertUser(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
+        $password = md5(md5(sha1(sha1($password))));
         $params = array(
             $username,
             $password,
@@ -20,6 +20,18 @@ class User_model extends CI_Model {
         $query = $this->db->query($sql, $params);
 
         $result = $query->row();
+
+        mysqli_next_result( $this->db->conn_id );
+
+        return $result;  // Assuming you expect a single result row
+    }
+
+    function getUserAccount($EmailAddress)
+    {
+        $sql = "Call getUserAccount(?)";
+        $params = array('EmailAddress' => $EmailAddress, );
+        $query = $this->db->query($sql, $params);
+        $result = $query->result();
 
         mysqli_next_result( $this->db->conn_id );
 
