0ae3225fb1e19e74fccd6cd8c28899c5e8976e5d
dracomalfoydaw <djbudoykokey@gmail.com>
2024-03-29 17:01:32 +0800

    init commit
    setup config here . once done, rename and remove the _setup word

---
+++ res_cons_setup.php
@@ -0,0 +1,19 @@
+<?php 
+define('CNF_APPNAME','Illana Bay Masonic Lodge No. 274 Masonic District RIX Zamboanga del Sur/Zamboanga Sibugay');
+define('CNF_APPNAME_CODE','monitoring');
+define('CNF_APPDESC','Monitoring');
+define('CNF_FOOTER','Monitoring');
+define('CNF_APPTITLE','Illana Bay Masonic Lodge No. 274 Masonic District RIX Zamboanga del Sur/Zamboanga Sibugay');
+define('CNF_APPNAMELANDINGPAGE','Illana Bay Masonic Lodge No. 274 Masonic District RIX Zamboanga del Sur/Zamboanga Sibugay');
+
+
+define('CNF_DBHOST','');
+define('CNF_DBUSERNAME','');
+define('CNF_DBPASS','');
+define('CNF_DBNAME','');
+define('CNF_DBDRIVER','');
+
+
+define('EmailAddress','');
+define('EmailPassword','');
+?>
