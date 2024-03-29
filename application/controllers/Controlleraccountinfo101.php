0d282e1406cca30ccc4fbf1a4dde894c449c6c70
dracomalfoydaw <djbudoykokey@gmail.com>
2024-03-29 16:58:20 +0800

    Update Controlleraccountinfo101.php
    patch

--- application/controllers/Controlleraccountinfo101.php
+++ application/controllers/Controlleraccountinfo101.php
@@ -59,10 +59,10 @@ class Controlleraccountinfo101 extends CI_Controller {
             $email = $this->htmlpurifier_lib->purify($this->input->post('email'));
             $userID = $this->htmlpurifier_lib->purify($this->input->post('userID'));
 
-
+            $groupID = $this->htmlpurifier_lib->purify($this->input->post('groupID'));
 
 	        // Call the model method to update the profile
-	        $result = $this->members->updateProfile($userID, $firstName, $middleName, $lastName, $nameExtension, $email);
+	        $result = $this->account->updateProfile($userID, $firstName, $middleName, $lastName,  $email,$groupID);
 
 	        $data = array(
 		        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
@@ -75,7 +75,7 @@ class Controlleraccountinfo101 extends CI_Controller {
 		    $message_details = validation_errors('<li>', '</li>');//'The following errors occurred <br>' . validation_errors('<li>', '</li>');
 		    $data = array(
 		        'message' => "error",
-		        'message_details' =>  json_encode($_POST),//$message_details,
+		        'message_details' =>  $message_details ,//$message_details,
 		        'id' => '',
 		    );
 		}
@@ -83,51 +83,72 @@ class Controlleraccountinfo101 extends CI_Controller {
 		echo json_encode($data);
 	}
 
+
+
 	public function savetransactioninfo()
 	{
 
 		$this->form_validation->set_rules('firstName', 'First Name', 'required|trim|htmlspecialchars');
-		
 		$this->form_validation->set_rules('lastName', 'Last Name', 'required|trim|htmlspecialchars');
-		$this->form_validation->set_rules('nameextension', 'Name Extension', 'required|trim|htmlspecialchars');
-		$this->form_validation->set_rules('email', 'Email', 'required|trim|htmlspecialchars|valid_email|is_unique[tb_profile.email]');
-		//$this->form_validation->set_rules('email', 'Email', 'required|trim|htmlspecialchars');
-		$this->form_validation->set_rules('idnumber', 'ID Number', 'required|trim|htmlspecialchars|is_unique[tb_profile.UniqueID]');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|htmlspecialchars|valid_email|is_unique[tb_users.email]');
+		$this->form_validation->set_rules('username', 'Username', 'required|trim|htmlspecialchars|is_unique[tb_users.username]');
+		$this->form_validation->set_rules('idnumber', 'ID Number', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]');
+        $this->form_validation->set_rules('confirmPassword', 'Confirm Password', 'required|matches[password]');
+        $this->form_validation->set_rules('group', 'Group', 'required');
+
 
 		// Check if form validation passed
 		if ($this->form_validation->run()) {
 		    // Form validation passed, no errors
-		    
-                    $firstName  = $this->htmlpurifier_lib->purify($this->input->post('firstName'));
-                    $middleName = $this->htmlpurifier_lib->purify($this->input->post('middleName'));
-                    $lastName = $this->htmlpurifier_lib->purify($this->input->post('lastName'));
-                    $nameExtension = $this->htmlpurifier_lib->purify($this->input->post('nameExtension'));
-                    
-                    $email = $this->htmlpurifier_lib->purify($this->input->post('email'));
-                    $idnumber = $this->htmlpurifier_lib->purify($this->input->post('idnumber'));
-                    $defaultuseraccount = $this->htmlpurifier_lib->purify($this->input->post('defaultuseraccount'));
+					$password =	$this->input->post('password');
+		    		$password_status = $this->filtering_process->password_check($password);
+		    		if($password_status['status']==true)
+		    		{
+		    				$firstName  = $this->htmlpurifier_lib->purify($this->input->post('firstName'));
+		                    $middleName = $this->htmlpurifier_lib->purify($this->input->post('middleName'));
+		                    $lastName = $this->htmlpurifier_lib->purify($this->input->post('lastName'));
+		                    $group = $this->htmlpurifier_lib->purify($this->input->post('group'));
+		                    if($group=="Superadmin")
+		                    {
+		                    	$group = 1;
+		                    }
+		                    elseif($group=="Admin")
+		                    {
+		                    	$group = 2;
+		                    }elseif($group=="Cashier")
+		                    {
+		                    	$group = 4;
+		                    }elseif($group=="Encoder")
+		                    {
+		                    	$group = 5;
+		                    }elseif($group=="Accounting")
+		                    {
+		                    	$group = 6;
+		                    }
+		                    else
+		                    {
+		                    	$group = 3;
+		                    }
+		                    $username = $this->htmlpurifier_lib->purify($this->input->post('username'));
+		                    $email = $this->htmlpurifier_lib->purify($this->input->post('email'));
+		                    $idnumber = $this->htmlpurifier_lib->purify($this->input->post('idnumber'));
+		                    $password = md5(md5(sha1(sha1($password))));
 
-
-                    $system_user = 1;
-                
-                    $result = $this->members->insertProfile($idnumber, $firstName, $middleName, $lastName, $nameExtension, $email);
-                    if($defaultuseraccount=="defaultsystemuser")
-                    {
-                    	$result = $this->User_model->insertUser($idnumber, $idnumber,  $email , 1, $result->ProfileID ,  $firstName  , $lastName, 1, $system_user);
-                    	$data = array(
-					        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
-					        'message_details' => '',
-					        
+		                    $result = $this->user_model->insertUser($username, $password,  $email , $group , $idnumber,  $firstName  , $lastName, 1, $this->session->userdata('uid'));
+	                    	$data = array(
+						        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
+						        'message_details' => '',
+						        
+						    );
+		    		}
+		    		else
+		    		{
+		    			$data = array(
+					        'message' =>  "error",  // Assuming success when validation passes
+					        'message_details' => $password_status->status_details,					        
 					    );
-                    }
-                    else
-                    {
-                    	$data = array(
-					        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
-					        'message_details' => '',
-					        
-					    );
-                    }
+		    		}
 
 		    
 		} else {
@@ -149,7 +170,59 @@ class Controlleraccountinfo101 extends CI_Controller {
 		$this->form_validation->set_rules('userID', 'User ID', 'required|trim|htmlspecialchars');
 		if ($this->form_validation->run()) {
 			$userID  = $this->htmlpurifier_lib->purify($this->input->post('userID'));
-			$result = $this->members->deleteProfile($userID);
+			$result = $this->account->deleteProfile($userID);
+			$data = array(
+		        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
+		        'message_details' => '',
+		        
+		    );
+		}
+		else
+		{
+			// Form validation failed, there are errors
+		    $message_details = validation_errors('<li>', '</li>');//'The following errors occurred <br>' . validation_errors('<li>', '</li>');
+		    $data = array(
+		        'message' => "error",
+		        'message_details' => $message_details,
+		        'id' => '',
+		    );
+		}
+		echo json_encode($data);
+	}
+
+	public function accountunlockinfo()
+	{
+		$this->form_validation->set_rules('userID', 'User ID', 'required|trim|htmlspecialchars');
+		if ($this->form_validation->run()) {
+			$userID  = $this->htmlpurifier_lib->purify($this->input->post('userID'));
+			$result = $this->account->accountunlockinfo($userID);
+			$data = array(
+		        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
+		        'message_details' => '',
+		        
+		    );
+		}
+		else
+		{
+			// Form validation failed, there are errors
+		    $message_details = validation_errors('<li>', '</li>');//'The following errors occurred <br>' . validation_errors('<li>', '</li>');
+		    $data = array(
+		        'message' => "error",
+		        'message_details' => $message_details,
+		        'id' => '',
+		    );
+		}
+		echo json_encode($data);
+	}
+	public function accountchangenewpasswordinfo()
+	{
+		$this->form_validation->set_rules('userID', 'User ID', 'required|trim|htmlspecialchars');		
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]');
+		if ($this->form_validation->run()) {
+			$userID  = $this->htmlpurifier_lib->purify($this->input->post('userID'));
+
+			$password = md5(md5(sha1(sha1($this->input->post('password')))));
+			$result = $this->account->accountchangenewpasswordinfo($userID, $password);
 			$data = array(
 		        'message' =>  $result->SuccessMessage,  // Assuming success when validation passes
 		        'message_details' => '',
