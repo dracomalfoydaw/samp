3056e77668c6338224f0f23e54984167857c4861
dracomalfoydaw <djbudoykokey@gmail.com>
2024-03-29 16:58:54 +0800

    Update Controlleraccountinginfo101.php
    added update transaction

--- application/controllers/Controlleraccountinginfo101.php
+++ application/controllers/Controlleraccountinginfo101.php
@@ -21,11 +21,152 @@ class Controlleraccountinginfo101 extends CI_Controller {
 		redirect('accounting/assessment',301);
 	}
 
+	public function updateListofPayment()
+	{
+		$this->form_validation->set_rules('amount', 'Amount Fees', 'required|trim|htmlspecialchars');
+		$this->form_validation->set_rules('EntryID', 'Fees', 'required|trim|htmlspecialchars');
+		$this->form_validation->set_rules('memberID', 'memberID', 'required|trim|htmlspecialchars');
+		if ($this->form_validation->run()) {
+				$amount = $this->htmlpurifier_lib->purify($this->input->post('amount')) ;
+				$EntryID = $this->htmlpurifier_lib->purify($this->input->post('EntryID')) ;
+				$memberID = $this->htmlpurifier_lib->purify($this->input->post('memberID')) ;
+				$Remarks = $this->htmlpurifier_lib->purify($this->input->post('Remarks')) ;
+
+				$payments = $this->accounting_model->UpdateCreditonJournal($EntryID, $amount,$Remarks);
+				
+				$message_details = "";
+				$data = array(
+			        'message' => $payments->SuccessMessage,
+			        'message_details' => '',
+			    );
+		}
+		else
+		{
+			$message_details = validation_errors('<li>', '</li>');
+		    	$data = array(
+			        'message' => "error",
+			        'message_details' => $message_details,
+			        'message_result' => '',
+			    );
+
+
+		}
+		echo json_encode($data);			
+	}
+
+	public function insertnewpayment()
+	{
+		$this->form_validation->set_rules('data', 'Fees', 'required|trim|htmlspecialchars');
+		$this->form_validation->set_rules('typePayment', 'Category', 'required|trim|htmlspecialchars');
+		$this->form_validation->set_rules('memberID', 'memberID', 'required|trim|htmlspecialchars');
+		if ($this->form_validation->run()) {
+				$typePayment = $this->htmlpurifier_lib->purify($this->input->post('typePayment')) ;
+				$data = $this->htmlpurifier_lib->purify($this->input->post('data')) ;
+				$remarks = $this->htmlpurifier_lib->purify($this->input->post('remarks')) ;
+				$memberID = $this->htmlpurifier_lib->purify($this->input->post('memberID')) ;
+
+				$payments = $this->accounting_model->addNewPayment($typePayment, $data,$memberID, $this->session->userdata('uid'),$remarks);
+				$message_details = "";
+				$data = array(
+			        'message' => $payments->SuccessMessage,
+			        'message_details' => $typePayment,
+			        'message_result' => $payments ,
+			    );
+		}
+		else
+		{
+			$message_details = validation_errors('<li>', '</li>');
+		    	$data = array(
+			        'message' => "error",
+			        'message_details' => $message_details,
+			        'message_result' => '',
+			    );
+
+
+		}
+		echo json_encode($data);
+	}
+
+	public function removeListofPayment()
+	{
+		$this->form_validation->set_rules('selectedTransactions', 'Fees', 'required|trim|htmlspecialchars');
+		$this->form_validation->set_rules('memberID', 'memberID', 'required|trim|htmlspecialchars');
+		if ($this->form_validation->run()) {
+			$memberID = $this->htmlpurifier_lib->purify($this->input->post('memberID')) ;
+			if(isset($_POST['selectedTransactions'])) {
+			    // Split the comma-separated string into an array
+			    $selectedTransactions = explode(',', $_POST['selectedTransactions']);
+
+			    // Iterate over the array
+			    foreach ($selectedTransactions as $EntryID ) {
+			        // Assuming $this->htmlpurifier_lib->purify() and $memberID are defined elsewhere
+			        $EntryID = $this->htmlpurifier_lib->purify($EntryID);
+			        $this->accounting_model->deleteListofPayment($EntryID, $memberID);
+			    }
+
+			    $message = "success";
+			    $message_details = "";
+			}
+			else
+			{
+				$message = "error";
+				$message_details = "<li> No transaction occured</li>";
+			}
+			$data = array(
+			        'message' => $message,
+			        'message_details' => $message_details,
+			        'message_result' => '',
+			    );
+		}
+		else
+		{
+			$message_details = validation_errors('<li>', '</li>');
+		    	$data = array(
+			        'message' => "error",
+			        'message_details' => $message_details,
+			        'message_result' => '',
+			    );
+
+
+		}
+		echo json_encode($data);
+
+	}
+
+	public function getListofPaymenttoAdd()
+	{
+		$this->form_validation->set_rules('typePayment', 'Category', 'required|trim|htmlspecialchars');
+		if ($this->form_validation->run()) {
+				$typePayment = $this->htmlpurifier_lib->purify($this->input->post('typePayment')) ;
+				$searchvalue = $this->htmlpurifier_lib->purify($this->input->post('searchvalue')) ;
+
+				$payments = $this->accounting_model->getListofPaymenttoAdd($typePayment, $searchvalue);
+				$message_details = "";
+				$data = array(
+			        'message' => "success",
+			        'message_details' => $typePayment,
+			        'message_result' => $payments ,
+			    );
+		}
+		else
+		{
+			$message_details = validation_errors('<li>', '</li>');
+		    	$data = array(
+			        'message' => "error",
+			        'message_details' => $message_details,
+			        'message_result' => '',
+			    );
+
+
+		}
+		echo json_encode($data);
+	}
+
 	public function individual_assessment()
 	{
 		$this->data = [];
 		$this->data['pageTitle'] = "Accounting";
-		$this->data['pageSubtitle'] = "Assessment/Billing Module";
+		$this->data['pageSubtitle'] = "Account balances Module";
 		
 		$this->data['content'] = $this->load->view('accounting/assessment/home',$this->data, true );
 		$this->data['home_script'] = $this->load->view('accounting/assessment/home_script',$this->data, true );
