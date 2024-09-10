025708a1b476bb57f05f81b989db37bd5ab16c37
dracomalfoydaw <djbudoykokey@gmail.com>
2024-09-10 21:05:19 +0800

    Update details.js

--- assets/js/members/details.js
+++ assets/js/members/details.js
@@ -253,6 +253,7 @@ app.component('image-uploader', {
     }
   },
   template: `
+
     <div class="card mb-4">
       <div class="card-header">Personal Information Details</div>
       <div class="card-body">
@@ -290,8 +291,8 @@ app.component('image-uploader', {
             <label class="small mb-1" for="ProfileStatus">Profile Status:</label>
             <div class="col-md-12">
               <span class="form-control">
-                <label><input :disabled="isLoading" type="radio" name="ProfileStatus" value="active" v-model="form.ProfileStatus"> Active</label> &nbsp;
-                <label><input :disabled="isLoading" type="radio" name="ProfileStatus" value="inactive" v-model="form.ProfileStatus"> Inactive</label>
+                <label><input :disabled="isLoading" type="radio" name="ProfileStatus" value="Active" v-model="form.ProfileStatus"> Active</label> &nbsp;
+                <label><input :disabled="isLoading" type="radio" name="ProfileStatus" value="Inactive" v-model="form.ProfileStatus"> Inactive</label>
               </span>
             </div>
             <div v-if="errors.ProfileStatus" class="error">{{ errors.ProfileStatus }}</div>
@@ -347,8 +348,29 @@ app.component('account-address-card', {
                 Province:ini_HomeBaranggay,
                 Municipality:ini_HomeMuncity,
                 Barangay:ini_HomeProvince,
-                zipcode:ini_zipcode
+                zipcode:ini_zipcode,
+                Country:ini_Country,
 
+                sex:ini_Sex,
+                dateofbirth:ini_DateofBirth,
+                placeofbirth:ini_PlaceofBirth,
+                bloodtype:ini_Bloodtype,
+
+                contactno:ini_ContactNumber,
+                faxno:ini_FaxNumber,
+                homeno:ini_HomeNumber,
+                officeno:ini_OfficeNumber,
+
+                Occupation:ini_Occupation,
+                Education:ini_Education,
+                Employment:ini_Employment,
+                EmploymentAddress:ini_EmploymentAddress,
+
+                familykin:ini_familykin,
+                familyrelation:ini_familyrelation,
+                familyaddress:ini_familyaddress,
+                familynokids:ini_familynokids,
+                familykidsname:ini_familykidsname,
             },
             errorsForms : {},   
             isSubmit: false,
@@ -385,6 +407,29 @@ app.component('account-address-card', {
                 formdata.append('Barangay', this.newForm.Barangay);
                 formdata.append('ZipCode', this.newForm.zipcode);
 
+                formdata.append('sex', this.newForm.sex);
+                formdata.append('dateofbirth', this.newForm.dateofbirth);
+                formdata.append('placeofbirth', this.newForm.placeofbirth);
+                formdata.append('bloodtype', this.newForm.bloodtype);
+
+                formdata.append('Country', this.newForm.Country);
+
+                formdata.append('contactno', this.newForm.contactno);
+                formdata.append('faxno', this.newForm.faxno);
+                formdata.append('homeno', this.newForm.homeno);
+                formdata.append('officeno', this.newForm.officeno);
+
+                formdata.append('Occupation', this.newForm.Occupation);
+                formdata.append('Education', this.newForm.Education);
+                formdata.append('Employment', this.newForm.Employment);
+                formdata.append('EmploymentAddress', this.newForm.EmploymentAddress);
+
+                formdata.append('familykin', this.newForm.familykin);
+                formdata.append('familyrelation', this.newForm.familyrelation);
+                formdata.append('familyaddress', this.newForm.familyaddress);
+                formdata.append('familynokids', this.newForm.familynokids);
+                formdata.append('familykidsname', this.newForm.familykidsname);
+
                 var address = base_url + 'members/updateinfo';
                 axios.post(address, formdata,)
                 .then(response_server => {
@@ -417,18 +462,88 @@ app.component('account-address-card', {
         },
     },
      template: `
+    <form @submit.prevent="confirmSubmit">
+    <div class="card mt-4">                        
+            <div class="card-header"></div>
+            <div class="card-body">
+                
+                    <div class="mb-3">
+                      <div class ="row">
+                        <div class="col-md-3">
+                          <label for="sex" class="form-label">Sex</label>
+                          <select class="form-control required" id="sex" name="sex" v-model="newForm.sex">
+                              <option value="M">Male</option>
+                              <option value="F">Female</option>
+                          </select>
+                          <span v-if="errorsForms.sex" style="color: red;">{{ errorsForms.sex }}</span>
+                        </div>
+                        <div class="col-md-3">
+                          <label for="dateofbirth" class="form-label">Date of Birth</label>
+                          <input :disabled="isSubmit" type="date" class="form-control" id="dateofbirth" name="dateofbirth" v-model="newForm.dateofbirth" >
+                          <span v-if="errorsForms.dateofbirth" style="color: red;">{{ errorsForms.dateofbirth }}</span>
+                        </div>
+                        <div class="col-md-3">
+                          <label for="placeofbirth" class="form-label">Place of Birth</label>
+                          <input :disabled="isSubmit" type="text" class="form-control" id="placeofbirth" name="placeofbirth" v-model="newForm.placeofbirth" required>
+                          <span v-if="errorsForms.placeofbirth" style="color: red;">{{ errorsForms.placeofbirth }}</span>
+                        </div>
+                        <div class="col-md-3">
+                          <label for="bloodtype" class="form-label">Blood Type</label>
+                          <input :disabled="isSubmit" type="text" class="form-control" id="bloodtype" name="bloodtype" v-model="newForm.bloodtype" >
+                          <span v-if="errorsForms.bloodtype" style="color: red;">{{ errorsForms.bloodtype }}</span>
+                        </div>
+                      </div>
+                    </div>
+                   <div class="mb-3">
+                      <div class ="row">
+                        <div class="col-md-3">                      
+                          <label for="Occupation" class="form-label">Occupation</label>
+                          <input :disabled="isSubmit" type="text" class="form-control" id="Occupation" name="Occupation" v-model="newForm.Occupation" required>
+                          <span v-if="errorsForms.Occupation" style="color: red;">{{ errorsForms.Occupation }}</span>
+                        </div>
+
+                        <div class="col-md-3">                      
+                          <label for="Education" class="form-label">Education</label>
+                          <input :disabled="isSubmit" type="text" class="form-control" id="Education" name="Education" v-model="newForm.Education" required>
+                          <span v-if="errorsForms.Education" style="color: red;">{{ errorsForms.Education }}</span>
+                        </div>
+
+
+                        <div class="col-md-3">                      
+                          <label for="Employment" class="form-label">Employment</label>
+                          <input :disabled="isSubmit" type="text" class="form-control" id="Employment" name="Employment" v-model="newForm.Employment" required>
+                          <span v-if="errorsForms.Employment" style="color: red;">{{ errorsForms.Employment }}</span>
+                        </div>
+
+
+                        <div class="col-md-3">                      
+                          <label for="EmploymentAddress" class="form-label">Address</label>
+                          <input :disabled="isSubmit" type="text" class="form-control" id="EmploymentAddress" name="EmploymentAddress" v-model="newForm.EmploymentAddress" required>
+                          <span v-if="errorsForms.EmploymentAddress" style="color: red;">{{ errorsForms.EmploymentAddress }}</span>
+                        </div>
+                      </div>
+                    </div>
+                
+            </div>
+        
+    </div>
 
     <div class="card mt-4">
                         
                             <div class="card-header">Home Address</div>
                             <div class="card-body">
-                                <form @submit.prevent="confirmSubmit">
+                                
                                     <div class="mb-3">
                                       <div class ="row">
-                                        <div class="col-md-12">
-                                          <label for="homeaddress" class=" control-label text-left"> Home/Blkd</label>
+                                        <div class="col-md-6">
+                                          <label for="homeaddress" class=" control-label text-left"> Home Address</label>
                                           <input :disabled="isSubmit" type="text" class="form-control" id="homeaddress" name="homeaddress" v-model="newForm.homeaddress" >
                                           <span v-if="errorsForms.homeaddress" style="color: red;">{{ errorsForms.homeaddress }}</span>
+                                        </div>
+                                        <div class="col-md-6">
+                                          <label for="Country" class=" control-label text-left"> Country</label>
+                                          <input :disabled="isSubmit" type="text" class="form-control" id="Country" name="Country" v-model="newForm.Country" >
+                                          <span v-if="errorsForms.Country" style="color: red;">{{ errorsForms.Country }}</span>
                                         </div>
                                       </div>
                                     </div>
@@ -456,11 +571,84 @@ app.component('account-address-card', {
                                         </div>
                                       </div>
                                     </div>
-                                    <button type="submit" class="btn btn-primary" :disabled="isSubmit">Submit</button>
-                                </form>  
+
+                                    <div class="mb-3">
+                                      <div class ="row">
+                                        <div class="col-md-3">                      
+                                          <label for="contactno" class="form-label">Contact No</label>
+                                          <input :disabled="isSubmit" type="text" class="form-control" id="contactno" name="contactno" v-model="newForm.contactno" required>
+                                          <span v-if="errorsForms.contactno" style="color: red;">{{ errorsForms.contactno }}</span>
+                                        </div>
+
+                                        <div class="col-md-3">                      
+                                          <label for="faxno" class="form-label">Fax No</label>
+                                          <input :disabled="isSubmit" type="text" class="form-control" id="faxno" name="faxno" v-model="newForm.faxno" required>
+                                          <span v-if="errorsForms.faxno" style="color: red;">{{ errorsForms.faxno }}</span>
+                                        </div>
+
+
+                                        <div class="col-md-3">                      
+                                          <label for="homeno" class="form-label">Home Number</label>
+                                          <input :disabled="isSubmit" type="text" class="form-control" id="homeno" name="homeno" v-model="newForm.homeno" required>
+                                          <span v-if="errorsForms.homeno" style="color: red;">{{ errorsForms.homeno }}</span>
+                                        </div>
+
+
+                                        <div class="col-md-3">                      
+                                          <label for="officeno" class="form-label">Office Number</label>
+                                          <input :disabled="isSubmit" type="text" class="form-control" id="officeno" name="officeno" v-model="newForm.officeno" required>
+                                          <span v-if="errorsForms.officeno" style="color: red;">{{ errorsForms.officeno }}</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                   
+                               
                             </div>
                         
                     </div>
+            <div class="card mt-4">                        
+                    <div class="card-header">Family Information</div>
+                    <div class="card-body">
+                        
+                            <div class="mb-3">
+                              <div class ="row">
+                                <div class="col-md-3">                      
+                                  <label for="familykin" class="form-label">Kin</label>
+                                  <input :disabled="isSubmit" type="text" class="form-control" id="familykin" name="familykin" v-model="newForm.familykin" required>
+                                  <span v-if="errorsForms.familykin" style="color: red;">{{ errorsForms.familykin }}</span>
+                                </div>
+                                <div class="col-md-3">                      
+                                  <label for="familyrelation" class="form-label">Relation</label>
+                                  <input :disabled="isSubmit" type="text" class="form-control" id="familyrelation" name="familyrelation" v-model="newForm.familyrelation" required>
+                                  <span v-if="errorsForms.familyrelation" style="color: red;">{{ errorsForms.familyrelation }}</span>
+                                </div>
+                                <div class="col-md-6">                      
+                                  <label for="familyaddress" class="form-label">Address</label>
+                                  <input :disabled="isSubmit" type="text" class="form-control" id="familyaddress" name="familyaddress" v-model="newForm.familyaddress" required>
+                                  <span v-if="errorsForms.familyaddress" style="color: red;">{{ errorsForms.familyaddress }}</span>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="mb-3">
+                              <div class ="row">
+                                <div class="col-md-3">                      
+                                  <label for="familynokids" class="form-label">Number of Kids</label>
+                                  <input :disabled="isSubmit" type="number" class="form-control" id="familynokids" name="familynokids" v-model="newForm.familynokids" required>
+                                  <span v-if="errorsForms.familynokids" style="color: red;">{{ errorsForms.familynokids }}</span>
+                                </div>
+                                <div class="col-md-9">                      
+                                  <label for="familykidsname" class="form-label">Name of Kids</label>
+                                  <input :disabled="isSubmit" type="text" class="form-control" id="familykidsname" name="familykidsname" v-model="newForm.familykidsname" required>
+                                  <span v-if="errorsForms.familykidsname" style="color: red;">{{ errorsForms.familykidsname }}</span>
+                                </div>
+                              </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary" :disabled="isSubmit">Submit</button>
+                        
+                    </div>
+                
+            </div>
+         </form>  
   `
 });
 
